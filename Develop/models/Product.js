098d9ee7b0021f07ace11a8f157f929ba5f52651
micahwaweru <micah.waweru@gmail.com>
2021-10-29 14:06:56 -0500

    correct product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,15 +10,37 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-    id:{
-      type:DataTypes.INTEGER,
-      allowNull:false,
-      primaryKey:true,
-      autoIncrement:true
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    category_name:{
-      type:DataTypes.STRING,
-      allowNull:false
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
