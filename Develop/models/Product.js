1ce36dd2d2410b789625748564913bf4aabdd9d9
micahwaweru <micah.waweru@gmail.com>
2021-10-29 13:54:54 -0500

    add models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +10,16 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    category_name:{
+      type:DataTypes.STRING,
+      allowNull:false
+    }
   },
   {
     sequelize,
